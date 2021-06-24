fea56995aee26773c8f480395242065963333044
Vikrant Chandra <58190465+Viku-51@users.noreply.github.com>
2021-06-25 00:16:49 +0530

    Create Credentials.py

---
+++ Credentials.py
@@ -0,0 +1,6 @@
+USER_NAME = 'user_id'   #I have Intentially hidden the username,password and api key due to safety reason(open source)
+PWD = 'your_password'
+API_KEY = 'api_key'
+FEED_TOKEN = None
+TOKEN_MAP = None
+
