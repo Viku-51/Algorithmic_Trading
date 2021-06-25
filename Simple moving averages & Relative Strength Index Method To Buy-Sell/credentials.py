94da49c9110a2a627e2aa8ee23ba26c94c1b7dea
Vikrant Chandra <58190465+Viku-51@users.noreply.github.com>
2021-06-25 14:42:50 +0530

    Add files via upload

---
+++ Simple moving averages & Relative Strength Index Method To Buy-Sell/credentials.py
@@ -0,0 +1,12 @@
+#replace  username,pwd, api_key  with yours
+USER_NAME = '6565'
+PWD = '6g!fdfgd'
+API_KEY = '657hgg'
+
+#Dont change  below varibale 
+FEED_TOKEN = None
+TOKEN_MAP = None
+SMART_API_OBJ = None
+
+
+
